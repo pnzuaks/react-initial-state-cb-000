f78ad4fd7f26c88f3eab63b122fa9eeed5f5918d
Patricia Nykaza <pnykaza@calstatela.edu>
2019-02-12 20:43:59 +0000

    Automatically backed up by Learn

--- src/components/Address.js
+++ src/components/Address.js
@@ -5,7 +5,7 @@ export default class Address extends React.Component {
   render() {
     return (
       <div className="address">
-      {this.props.street}
+      {this.props.street}, {this.props.city}
       </div>
     )
   }
