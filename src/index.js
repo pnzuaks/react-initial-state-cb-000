cec8129707be1c2558ae6c764e086d752231908f
Patricia Nykaza <pnykaza@calstatela.edu>
2019-02-12 20:38:28 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import ToggleButton from './components/ToggleButton';
+import Address from './components/Address';
+
 
 
 ReactDOM.render(
