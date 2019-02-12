c6052c4b2467806ca25ce3a9772e868e93d46a2f
Patricia Nykaza <pnykaza@calstatela.edu>
2019-02-12 20:43:37 +0000

    Automatically backed up by Learn

--- src/components/Address.js
+++ src/components/Address.js
@@ -2,14 +2,6 @@
 import React from 'react';
 
 export default class Address extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      fullAddress: `${props.street}, ${props.city}`
-    }
-  }
-
   render() {
     return (
       <div className="address">
