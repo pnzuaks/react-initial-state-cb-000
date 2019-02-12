c22c3b1cb739301f3836d2b89297853052d9bee0
Patricia Nykaza <pnykaza@calstatela.edu>
2019-02-12 20:37:53 +0000

    Automatically backed up by Learn

--- src/components/Address.js
+++ src/components/Address.js
@@ -12,7 +12,9 @@ export default class Address extends React.Component {
 
   render() {
     return (
-      
+      <div className="address">
+      {this.state.fullAddress}
+      </div>
     )
   }
 }
